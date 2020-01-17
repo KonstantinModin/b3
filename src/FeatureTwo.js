a9732e263e55d3dcebba5ed5e8713a7a5f78467e
KonstantinModin <konstantin.v.modin@gmail.com>
2020-01-17 07:19:49 +0100

    updates

--- src/FeatureTwo.js
+++ src/FeatureTwo.js
@@ -6,7 +6,7 @@ const FeatureTwo = () => {
     return (        
         <div className="feature-dark">
             <Row className="feature-content">                   
-                <Col lg={6} className="feature-caption">
+                <Col lg={6} className="feature-caption d-flex flex-sm-column justify-content-center">
                    <h6>Incredible feature</h6>
                    <h2>Totally random website. At the touch of your finger pads.</h2>
                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas cumque obcaecati quidem earum distinctio temporibus, delectus perferendis porro similique beatae tempora saepe aut omnis quo. Iusto ipsum dolores molestiae voluptates?</p>
