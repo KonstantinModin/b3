ef644743e326fc16e95d6616d4f1035f7f3845f9
KonstantinModin <konstantin.v.modin@gmail.com>
2020-01-20 08:17:43 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <>      
       <NavBarMain />
       <Stage />
-      {arr.map(({a,b,dark,order,hide},i)=>
+      {arr.map(({a, b, dark, order, hide}, i)=>
         <Feature key={i} col1={a} col2={b} dark={dark} order={order} hide={hide}/>
       )}      
     </>
