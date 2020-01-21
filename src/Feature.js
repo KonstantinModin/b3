4b391a194733c45a7495f67ef18765efe26151b6
KonstantinModin <konstantin.v.modin@gmail.com>
2020-01-21 06:25:57 +0100

    updates

--- src/Feature.js
+++ src/Feature.js
@@ -5,23 +5,24 @@ const Feature = ({ col1, col2, dark, order, hide}) => {
     const rowLeftClass = 
         [order?"feature-caption d-flex flex-column justify-content-center":"",
         hide&&order?"d-none d-md-block text-sm-center":""].join` `;
+    
     const rowRightClass = 
         [!order?"feature-caption d-flex flex-column justify-content-center":"",
         hide&&!order?"d-none d-md-block text-sm-center":""].join` `;
+    
     return (        
         <div className={dark?"feature-dark":""}>
             <Row className="feature-content">                   
                 <Col 
-                    lg={6} 
-                    className={rowLeftClass}
-                    >
-                    {order?col1:col2}
+                    lg={{span:order?6:5, order:2, offset:order?0:1}} 
+                    className={rowLeftClass}                    
+                >
+                    {col1}
                 </Col>
                 <Col 
-                    lg={dark?6:5} 
-                    md={{offset:dark?0:1}} 
+                    lg={{span:order?5:6, offset:order?1:0,order:order?3:1}}                     
                     className={rowRightClass}>
-                    {order?col2:col1}
+                    {col2}
                 </Col>                   
             </Row>
         </div>        
